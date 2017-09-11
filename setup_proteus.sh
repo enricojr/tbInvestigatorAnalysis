636efc33a150db52b8538e3ec6fb7a1de9d31a1a
Enrico Junior Schioppa <enrico.junior.schioppa@cern.ch>
2017-09-11 08:36:58 +0200

    added setup scripts

---
+++ setup_proteus.sh
@@ -0,0 +1,5 @@
+. /cvmfs/sft.cern.ch/lcg/releases/gcc/4.9.3/x86_64-slc6/setup.sh
+pushd /cvmfs/sft.cern.ch/lcg/app/releases/ROOT/5.34.36/x86_64-slc6-gcc49-opt/root
+. bin/thisroot.sh
+popd
+source proteus/build/activate.sh
---
+++ setup_tbConverter.sh
@@ -0,0 +1,3 @@
+export AtlasSetup=/afs/cern.ch/atlas/software/dist/AtlasSetup
+alias asetup='source $AtlasSetup/scripts/asetup.sh'
+asetup 19.2.1.6,64,here
