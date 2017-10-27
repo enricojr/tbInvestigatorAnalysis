fb3ec3f5401643f2cd2b22c54c6c5ffdd972ebe8
Enrico Junior Schioppa <enrico.junior.schioppa@cern.ch>
2017-10-27 18:20:29 +0200

    updates for summary

--- scripts/summary/summary.C
+++ scripts/summary/summary.C
@@ -1,6 +1,7 @@
 #include "summary.hh"
 
 int summary(const unsigned int runNumber,
+	    const char *fileNameCfg,
 	    const char *fileNameDUTConversion,
 	    const char *fileNameTelescopeConversion,
 	    const char *fileNameTelescopeAlignment,
@@ -8,7 +9,25 @@ int summary(const unsigned int runNumber,
 	    const char *fileNameSynchronization,
 	    const char *fileNamePositionCorrection,
 	    const char *fileNameEfficiency,
-	    const char *fileNameLatex){
+	    const char *fileNameLatex,
+	    const string plotPath){
+
+  gStyle->SetNumberContours(255);
+  gStyle -> SetPalette(55);
+  gStyle -> SetOptStat(0);
+  gStyle -> SetOptTitle(0);
+  gStyle -> SetOptFit(0);
+
+  /////////////////////////////
+  // loading configuration file
+  /////////////////////////////
+  cout << __PRETTY_FUNCTION__ << ": loading configuration file " << fileNameCfg << endl;
+  configClass *cfg = new configClass();
+  if(cfg -> load(fileNameCfg)){
+    cout << __PRETTY_FUNCTION__ << ": ERROR!!! - cannot load configuration" << endl;
+    return 1;
+  }
+  cfg -> print();
 
   /////////////////////
   // opening Latex file
@@ -16,7 +35,7 @@ int summary(const unsigned int runNumber,
   cout << __PRETTY_FUNCTION__ << ": opening file " << fileNameLatex << endl;
   ofstream fileLatex(fileNameLatex);
   if(!fileLatex){
-    cout << __PRETTY_FUNCTION__ << ": ERROR!!! - cannot open file " << fileNameLatex << endl;
+    cout << __PRETTY_FUNCTION__ << ": ERROR!!! - cannot open Latex file " << endl;
     return 1;
   }
 
@@ -24,44 +43,170 @@ int summary(const unsigned int runNumber,
   // initializing Latex file
   //////////////////////////
   cout << __PRETTY_FUNCTION__ << ": initializing Latex file " << endl;
-  fileLatex << "\\documentclass[a4paper,10pt,titlepage,openright ]{book}\n" << endl;
-  fileLatex << "\\usepackage{verbatim} % to use multiline comments: \begin{comment}\n" << endl;
-  fileLatex << "\\usepackage{graphicx} % to use \includegraphics\n" << endl;
-  fileLatex << "\\usepackage{caption} % to use subfigures\n" << endl;
-  fileLatex << "\\usepackage{subcaption}\usepackage{amsmath} % to use mathematics\n" << endl;
-  fileLatex << "\\usepackage{wrapfig} % to use figures embedded in the text\n" << endl;
-  fileLatex << "\\usepackage{hyperref}\n" << endl;
-  fileLatex << "\\newcommand{\HRule}{\rule{\linewidth}{0.5mm}} % for title page\n" << endl;
-  fileLatex << "\\usepackage{enumerate}\n" << endl;
-  fileLatex << "\\usepackage{xfrac} % to use sfrac, slanted fractions\n" << endl;
-  fileLatex << "\\usepackage{fancyhdr} % to enable fancy header style\n" << endl;
-  fileLatex << "\\usepackage[margin=0.5in]{geometry}\n" << endl;
-  fileLatex << "\\usepackage{array,multirow} % for tables\n" << endl;
-  fileLatex << "\\usepackage[version=3]{mhchem} % for chemical formulas\n" << endl;
-  fileLatex << "\\usepackage{textcomp} % to use angular brackets in text\n" << endl;
-  fileLatex << "\\usepackage{xcolor}\n" << endl;
-  fileLatex << "\\usepackage{wrapfig}\n" << endl;
-  fileLatex << "\\usepackage{enumitem} % to resume counters in enumerate\n" << endl;
-  fileLatex << "\\usepackage{upgreek}\n" << endl;
-  fileLatex << "\\usepackage{float}\n" << endl;
-  fileLatex << "\\usepackage{listings} % to write C++ code\n" << endl;
-  fileLatex << "\\usepackage{slashed}\n" << endl;
-  fileLatex << "\\usepackage{mathtools} % to use def above equal sign\n" << endl;
-  fileLatex << "\\usepackage{soul} % to underline text and still have new lines\n" << endl;
-  fileLatex << "\\usepackage{xcolor} % to color-underline in equations\n" << endl;
-  fileLatex << "\\usepackage[makeroom]{cancel} % to cross-out terms in formulas\n" << endl;
-  fileLatex << "\\begin{document}\n" << endl;
+  fileLatex << "\\documentclass[a4paper,10pt,titlepage,openright ]{article}" << endl;
+  fileLatex << "\\usepackage{verbatim} % to use multiline comments: \\begin{comment}" << endl;
+  fileLatex << "\\usepackage{graphicx} % to use \\includegraphics" << endl;
+  fileLatex << "\\usepackage{caption} % to use subfigures" << endl;
+  fileLatex << "\\usepackage{subcaption}\\usepackage{amsmath} % to use mathematics" << endl;
+  fileLatex << "\\usepackage{wrapfig} % to use figures embedded in the text" << endl;
+  fileLatex << "\\usepackage{hyperref}" << endl;
+  fileLatex << "\\newcommand{\\HRule}{\\rule{\\linewidth}{0.5mm}} % for title page" << endl;
+  fileLatex << "\\usepackage{enumerate}" << endl;
+  fileLatex << "\\usepackage{xfrac} % to use sfrac, slanted fractions" << endl;
+  fileLatex << "\\usepackage{fancyhdr} % to enable fancy header style" << endl;
+  fileLatex << "\\usepackage[margin=0.5in]{geometry}" << endl;
+  fileLatex << "\\usepackage{array,multirow} % for tables" << endl;
+  fileLatex << "\\usepackage[version=3]{mhchem} % for chemical formulas" << endl;
+  fileLatex << "\\usepackage{textcomp} % to use angular brackets in text" << endl;
+  fileLatex << "\\usepackage{xcolor}" << endl;
+  fileLatex << "\\usepackage{wrapfig}" << endl;
+  fileLatex << "\\usepackage{enumitem} % to resume counters in enumerate" << endl;
+  fileLatex << "\\usepackage{upgreek}" << endl;
+  fileLatex << "\\usepackage{float}" << endl;
+  fileLatex << "\\usepackage{listings} % to write C++ code" << endl;
+  fileLatex << "\\usepackage{slashed}" << endl;
+  fileLatex << "\\usepackage{mathtools} % to use def above equal sign" << endl;
+  fileLatex << "\\usepackage{soul} % to underline text and still have new lines" << endl;
+  fileLatex << "\\usepackage{xcolor} % to color-underline in equations" << endl;
+  fileLatex << "\\usepackage[makeroom]{cancel} % to cross-out terms in formulas" << endl;
+  fileLatex << "\\begin{document}" << endl;
+
+  /////////////
+  // title page
+  /////////////
+  cout << __PRETTY_FUNCTION__ << ": writing title page " << endl;
+  char title[1000];
+  sprintf(title, "RUN %06d", runNumber);
+  fileLatex << "\\begin{titlepage}" << endl;
+  fileLatex << "\\centering" << endl;
+  fileLatex << "{\\scshape\\LARGE CERN EP-ADE-ID \\par}" << endl;
+  fileLatex << "\\vspace{1cm}" << endl;
+  fileLatex << "{\\scshape\\Large TJ-Investigator test beam data report\\par}" << endl;
+  fileLatex << "\\vspace{1.5cm}" << endl;
+  fileLatex << "{\\huge\\bfseries " << title << " \\par}" << endl;
+  fileLatex << "\\vspace{2cm}" << endl;
+  fileLatex << "{\\Large\\itshape Dataset CERN SPS, May-October 2017\\par}" << endl;
+  fileLatex << "\\vfill" << endl;
+  fileLatex << "edited by\\par" << endl;
+  fileLatex << "Enrico Jr. \\textsc{Schioppa}\\par" << endl;
+  fileLatex << "Florian \\textsc{Dachs}\\par" << endl;
+  fileLatex << "Maria \\textsc{Moreno Llacer}\\par" << endl;
+  fileLatex << "Simone \\textsc{Monzani}" << endl;
+  fileLatex << "\\vfill" << endl;
+  fileLatex << "{\\large \\today\\par}" << endl;
+  fileLatex << "\\title{" << title << "}" << endl;
+  fileLatex << "\\end{titlepage}" << endl;
+
+  ///////////
+  // overview
+  ///////////
+  cout << __PRETTY_FUNCTION__ << ": writing overview " << endl;
+  fileLatex << "\\section{Overview}" << endl;
+  fileLatex << "{\\bf Run number                               :} " << runNumber << "\\\\" << endl;
+  int nDUTHits = getNumberOfDUTHits(fileNameDUTConversion);
+  if(nDUTHits < 0){
+    cout << __PRETTY_FUNCTION__ << ": ERROR!!! - cannot get number of DUT hits" << endl;
+    return 1;
+  }
+  fileLatex << "{\\bf Number of DUT hits (before any selection):} " << nDUTHits << "\\\\" << endl;
+  int nTelescopeHits = getNumberOfTelescopeHits(fileNameTelescopeConversion);
+  if(nTelescopeHits < 0){
+    cout << __PRETTY_FUNCTION__ << ": ERROR!!! - cannot get number of Telescope hits" << endl;
+    return 1;
+  }
+  fileLatex << "{\\bf Number of Telescope hits (before any selection):} " << nTelescopeHits << "\\\\" << endl;
+  fileLatex << "{\\bf Exceeding number of DUT hits (before any selection):} " << -nTelescopeHits+nDUTHits << "\\\\" << endl;
+  fileLatex << "{\\bf Exceeding fraction of DUT hits (before any selection):} " << 100.*(-nTelescopeHits+nDUTHits)/nDUTHits << "\\% \\\\" << endl;
+  fileLatex << "{\\bf Number of selected DUT hits:} " << "to be added" << "\\\\" << endl;
+  fileLatex << "{\\bf Fraction of selected DUT hits:} " << "to be added" << "\\\\" << endl;
+  fileLatex << "{\\bf Synchronization fraction:} " << "to be added" << "\\\\" << endl;
+  fileLatex << "{\\bf Input file for DUT conversion            :} \\\\ " << escapeSomeChars(fileNameDUTConversion) << "\\\\" << endl;
+  fileLatex << "{\\bf Input file for telescope conversion      :} \\\\ " << escapeSomeChars(fileNameTelescopeConversion) << "\\\\" << endl;
+  fileLatex << "{\\bf Input file for telescope alignment       :} \\\\ " << escapeSomeChars(fileNameTelescopeAlignment) << "\\\\" << endl;
+  fileLatex << "{\\bf Input file for telescope tracking        :} \\\\ " << escapeSomeChars(fileNameTelescopeTracking) << "\\\\" << endl;
+  fileLatex << "{\\bf Input file for synchronization           :} \\\\ " << escapeSomeChars(fileNameSynchronization) << "\\\\" << endl;
+  fileLatex << "{\\bf Input file for position correction       :} \\\\ " << escapeSomeChars(fileNamePositionCorrection) << "\\\\" << endl;
+  fileLatex << "{\\bf Input file for efficiency                :} \\\\ " << escapeSomeChars(fileNameEfficiency) << "\\\\" << endl;
+
+  /////////////////
+  // DUT conversion
+  /////////////////
+  cout << __PRETTY_FUNCTION__ << ": writing DUT conversion " << endl;
+  if(writeDUTConversion(cfg, fileLatex, fileNameDUTConversion, plotPath)){
+    cout << __PRETTY_FUNCTION__ << ": ERROR!!! - cannot write DUT conversion" << endl;
+    return 0;
+  }
+
+  ///////////////////////
+  // Telescope conversion
+  ///////////////////////
+  cout << __PRETTY_FUNCTION__ << ": writing telescope conversion " << endl;
+  if(writeTelescopeConversion(fileLatex, fileNameTelescopeConversion, plotPath)){
+    cout << __PRETTY_FUNCTION__ << ": ERROR!!! - cannot write Telescope conversion" << endl;
+    return 0;
+  }
+
+  //////////////////////
+  // Telescope alignment
+  //////////////////////
+  cout << __PRETTY_FUNCTION__ << ": writing telescope alignment " << endl;
+  if(writeTelescopeAlignment(fileLatex, fileNameTelescopeAlignment, plotPath)){
+    cout << __PRETTY_FUNCTION__ << ": ERROR!!! - cannot write Telescope alignment" << endl;
+    return 0;
+  }
+
+  /////////////////////
+  // Telescope tracking
+  /////////////////////
+  cout << __PRETTY_FUNCTION__ << ": writing telescope tracking " << endl;
+  if(writeTelescopeTracking(fileLatex, fileNameTelescopeTracking, plotPath)){
+    cout << __PRETTY_FUNCTION__ << ": ERROR!!! - cannot write Telescope tracking" << endl;
+    return 0;
+  }
+
+  //////////////////
+  // Synchronization
+  //////////////////
+  cout << __PRETTY_FUNCTION__ << ": writing synchronization " << endl;
+  if(writeSynchronization(fileLatex, fileNameSynchronization, plotPath)){
+    cout << __PRETTY_FUNCTION__ << ": ERROR!!! - cannot write synchronization" << endl;
+    return 0;
+  }
+
+  //////////////////////
+  // Position correction
+  //////////////////////
+  cout << __PRETTY_FUNCTION__ << ": writing position correction " << endl;
+  if(writePositionCorrection(fileLatex, fileNamePositionCorrection, plotPath)){
+    cout << __PRETTY_FUNCTION__ << ": ERROR!!! - cannot write position correction" << endl;
+    return 0;
+  }
+
+  //////////////////
+  // Efficiency
+  //////////////////
+  cout << __PRETTY_FUNCTION__ << ": writing efficiency " << endl;
+  if(writeEfficiency(fileLatex, fileNameEfficiency, plotPath)){
+    cout << __PRETTY_FUNCTION__ << ": ERROR!!! - cannot write efficiency" << endl;
+    return 0;
+  }
 
   ////////////////////////
   // finalizing Latex file
   ////////////////////////
   cout << __PRETTY_FUNCTION__ << ": finalizing Latex file" << endl;
-  fileLatex << "\\end{document}\n" << endl;
+  fileLatex << "\\end{document}" << endl;
   
   /////////////////////
   // closing Latex file
   /////////////////////
+  cout << __PRETTY_FUNCTION__ << ": closing Latex file" << endl;
   fileLatex.close();
-  
+
+  /////////
+  // ending
+  /////////
+  cout << __PRETTY_FUNCTION__ << ": ending" << endl;
+  delete cfg;
   return 0;
 }
--- scripts/summary/summary.hh
+++ scripts/summary/summary.hh
@@ -1,6 +1,329 @@
 #ifndef SUMMARY_HH
 #define SUMMARY_HH
 
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <iterator>
+#include <set>
+#include <sstream>
+#include <vector>
+using namespace std;
 
+#include <TFile.h>
+#include <TTree.h>
+#include <TCanvas.h>
+#include <TGraph.h>
+#include <TGraphErrors.h>
+#include <TH2.h>
+#include <TH1.h>
+#include <TLegend.h>
+#include <TAxis.h>
+#include <TStyle.h>
+#include <TColor.h>
+#include <TPad.h>
+#include <TLine.h>
+
+#include "../aux/configClass.hh"
+
+#define NDUTCHANNELS 5
+
+template <class _II, class _OI>
+static _OI escapeSomeChars(const _II inIt, const _II endIt, _OI outIt)
+{
+  for (_II it=inIt; it!=endIt; ++it)
+    switch (*it)
+      {
+      case '\0': outIt++ = '\\'; outIt++ = '0'; break;
+      case '\n': outIt++ = '\\'; outIt++ = 'n'; break;
+      case '_': outIt++ = '\\'; outIt++ = '_'; break;
+      case '"' : 
+      case '$' : 
+      default  : outIt++ = *it;
+      }
+
+  return outIt;
+}
+
+static std::string escapeSomeChars(const std::string& input)
+{
+  std::ostringstream os;
+  escapeSomeChars(input.begin(), input.end(), std::ostream_iterator<char>(os));
+  return os.str();
+}
+
+namespace /*anon*/ {
+  struct rawchar {   // helper - see e.g. http://bytes.com/topic/c/answers/436124-copy-istream_iterator-question
+    char _c; rawchar(char c=0) : _c(c) {} 
+    operator const char&() const { return _c; }
+    friend std::istream& operator>>(std::istream& is, rawchar& out) { return is.get(out._c); }
+  };
+}
+
+int getNumberOfDUTHits(const char *fileNameDUTConversion){
+  int nHits = -1;
+  TFile *file = TFile::Open(fileNameDUTConversion);
+  if(file == NULL) cout << __PRETTY_FUNCTION__ << ": ERROR!!! - cannot open DUT conversion file" << endl;
+  else{
+    TTree *tree = (TTree *) file -> Get("DUTTree");
+    if(tree == NULL) cout << __PRETTY_FUNCTION__ << ": ERROR!!! - cannot open DUT conversion tree" << endl;
+    else{
+      nHits = tree -> GetEntries() / NDUTCHANNELS;
+      delete tree;
+    }
+    file -> Close();
+    delete file;
+  }
+
+  return nHits;
+}
+
+int getNumberOfTelescopeHits(const char *fileNameTelescopeConversion){
+  int nHits = -1;
+  TFile *file = TFile::Open(fileNameTelescopeConversion);
+  if(file == NULL) cout << __PRETTY_FUNCTION__ << ": ERROR!!! - cannot open Telescope conversion file" << endl;
+  else{
+    TTree *tree = (TTree *) file -> Get("Event");
+    if(tree == NULL) cout << __PRETTY_FUNCTION__ << ": ERROR!!! - cannot open Telescope conversion tree" << endl;
+    else{
+      nHits = tree -> GetEntries();
+      delete tree;
+    }
+    file -> Close();
+    delete file;
+  }
+
+  return nHits;
+}
+
+int writeDUTConversion(configClass *cfg,
+		       ofstream &fileLatex, 
+		       const char *fileNameDUTConversion, 
+		       const string plotPath){
+
+  fileLatex << "\\newpage" << endl;
+  fileLatex << "\\section{DUT conversion}" << endl;
+
+  gStyle -> SetOptTitle(1);
+
+  TFile *file = TFile::Open(fileNameDUTConversion);
+  if(file == NULL){
+    cout << __PRETTY_FUNCTION__ << ": ERROR!!! - cannot open DUT conversion file" << endl;
+    return 1;
+  }
+
+  TLine *line = NULL;
+  TCanvas *cc = new TCanvas("cc", "cc", 0, 0, 2000, 1000);
+
+  vector<pair<unsigned int, unsigned int> > channel;
+  channel.push_back(pair<unsigned int, unsigned int> (0, 0));
+  channel.push_back(pair<unsigned int, unsigned int> (0, 1));
+  channel.push_back(pair<unsigned int, unsigned int> (0, 2));
+  channel.push_back(pair<unsigned int, unsigned int> (1, 0));
+
+  for(unsigned int iChannel=0; iChannel<channel.size(); iChannel++){
+
+    stringstream DRS;
+    DRS << channel[iChannel].first;
+    stringstream CH;
+    CH << channel[iChannel].second;    
+
+    const string plotNameLinearExcludedPdf = "/DUTConversion_h2LinearExcludedWaveforms_DRS_" + DRS.str() + "_CH_" + CH.str() + ".pdf";
+    const string plotNameLinearExcludedRoot = "/DUTConversion_h2LinearExcludedWaveforms_DRS_" + DRS.str() + "_CH_" + CH.str() + ".root";
+    const string plotNameLinearSelectedPdf = "/DUTConversion_h2LinearSelectedWaveforms_DRS_" + DRS.str() + "_CH_" + CH.str() + ".pdf";
+    const string plotNameLinearSelectedRoot = "/DUTConversion_h2LinearSelectedWaveforms_DRS_" + DRS.str() + "_CH_" + CH.str() + ".root";
+    const string plotNamePulseExcludedPdf = "/DUTConversion_h2PulseExcludedWaveforms_DRS_" + DRS.str() + "_CH_" + CH.str() + ".pdf";
+    const string plotNamePulseExcludedRoot = "/DUTConversion_h2PulseExcludedWaveforms_DRS_" + DRS.str() + "_CH_" + CH.str() + ".root";
+    const string plotNamePulseSelectedPdf = "/DUTConversion_h2PulseSelectedWaveforms_DRS_" + DRS.str() + "_CH_" + CH.str() + ".pdf";
+    const string plotNamePulseSelectedRoot = "/DUTConversion_h2PulseSelectedWaveforms_DRS_" + DRS.str() + "_CH_" + CH.str() + ".root";
+
+    TH2F *h2LinearExcludedWaveforms_ = (TH2F *) file -> Get(("h2LinearExcludedWaveforms_DRS_" + DRS.str() + "_CH_" + CH.str()).c_str());
+    h2LinearExcludedWaveforms_ -> GetXaxis() -> SetNdivisions(712);
+    h2LinearExcludedWaveforms_ -> GetXaxis() -> SetTitle("Time bin");
+    h2LinearExcludedWaveforms_ -> GetYaxis() -> SetTitle("Signal [mV]");
+    h2LinearExcludedWaveforms_ -> GetYaxis() -> SetTitleOffset(1.2);
+    h2LinearExcludedWaveforms_ -> Draw("colz");  
+    cc -> Update();
+    line = new TLine(cfg -> _pulseT0[channel[iChannel].first][channel[iChannel].second].val, gPad->GetUymin(),
+		     cfg -> _pulseT0[channel[iChannel].first][channel[iChannel].second].val, gPad->GetUymax());
+    line -> SetLineColor(2);
+    line -> SetLineWidth(2);
+    line -> Draw();
+    line = new TLine(cfg -> _pulseT0[channel[iChannel].first][channel[iChannel].second].range.min, gPad->GetUymin(),
+		     cfg -> _pulseT0[channel[iChannel].first][channel[iChannel].second].range.min, gPad->GetUymax());
+    line -> SetLineColor(3);
+    line -> SetLineWidth(2);
+    line -> Draw();
+    line = new TLine(cfg -> _pulseT0[channel[iChannel].first][channel[iChannel].second].range.max, gPad->GetUymin(),
+		     cfg -> _pulseT0[channel[iChannel].first][channel[iChannel].second].range.max, gPad->GetUymax());
+    line -> SetLineColor(3);
+    line -> SetLineWidth(2);
+    line -> Draw();
+    cc -> SaveAs((plotPath+plotNameLinearExcludedPdf).c_str());
+    cc -> SaveAs((plotPath+plotNameLinearExcludedRoot).c_str());
+
+    TH2F *h2LinearSelectedWaveforms_ = (TH2F *) file -> Get(("h2LinearSelectedWaveforms_DRS_" + DRS.str() + "_CH_" + CH.str()).c_str());
+    h2LinearSelectedWaveforms_ -> GetXaxis() -> SetNdivisions(712);
+    h2LinearSelectedWaveforms_ -> GetXaxis() -> SetTitle("Time bin");
+    h2LinearSelectedWaveforms_ -> GetYaxis() -> SetTitle("Signal [mV]");
+    h2LinearSelectedWaveforms_ -> GetYaxis() -> SetTitleOffset(1.2);
+    h2LinearSelectedWaveforms_ -> Draw("colz");  
+    cc -> Update();
+    line = new TLine(cfg -> _pulseT0[channel[iChannel].first][channel[iChannel].second].val, gPad->GetUymin(),
+		     cfg -> _pulseT0[channel[iChannel].first][channel[iChannel].second].val, gPad->GetUymax());
+    line -> SetLineColor(2);
+    line -> SetLineWidth(2);
+    line -> Draw();
+    line = new TLine(cfg -> _pulseT0[channel[iChannel].first][channel[iChannel].second].range.min, gPad->GetUymin(),
+		     cfg -> _pulseT0[channel[iChannel].first][channel[iChannel].second].range.min, gPad->GetUymax());
+    line -> SetLineColor(3);
+    line -> SetLineWidth(2);
+    line -> Draw();
+    line = new TLine(cfg -> _pulseT0[channel[iChannel].first][channel[iChannel].second].range.max, gPad->GetUymin(),
+		     cfg -> _pulseT0[channel[iChannel].first][channel[iChannel].second].range.max, gPad->GetUymax());
+    line -> SetLineColor(3);
+    line -> SetLineWidth(2);
+    line -> Draw();
+    cc -> SaveAs((plotPath+plotNameLinearSelectedPdf).c_str());
+    cc -> SaveAs((plotPath+plotNameLinearSelectedRoot).c_str());
+
+    TH2F *h2PulseExcludedWaveforms_ = (TH2F *) file -> Get(("h2PulseExcludedWaveforms_DRS_" + DRS.str() + "_CH_" + CH.str()).c_str());
+    h2PulseExcludedWaveforms_ -> GetXaxis() -> SetNdivisions(712);
+    h2PulseExcludedWaveforms_ -> GetXaxis() -> SetTitle("Time bin");
+    h2PulseExcludedWaveforms_ -> GetYaxis() -> SetTitle("Signal [mV]");
+    h2PulseExcludedWaveforms_ -> GetYaxis() -> SetTitleOffset(1.2);
+    h2PulseExcludedWaveforms_ -> Draw("colz");  
+    cc -> Update();
+    line = new TLine(cfg -> _pulseT0[channel[iChannel].first][channel[iChannel].second].val, gPad->GetUymin(),
+		     cfg -> _pulseT0[channel[iChannel].first][channel[iChannel].second].val, gPad->GetUymax());
+    line -> SetLineColor(2);
+    line -> SetLineWidth(2);
+    line -> Draw();
+    line = new TLine(cfg -> _pulseT0[channel[iChannel].first][channel[iChannel].second].range.min, gPad->GetUymin(),
+		     cfg -> _pulseT0[channel[iChannel].first][channel[iChannel].second].range.min, gPad->GetUymax());
+    line -> SetLineColor(3);
+    line -> SetLineWidth(2);
+    line -> Draw();
+    line = new TLine(cfg -> _pulseT0[channel[iChannel].first][channel[iChannel].second].range.max, gPad->GetUymin(),
+		     cfg -> _pulseT0[channel[iChannel].first][channel[iChannel].second].range.max, gPad->GetUymax());
+    line -> SetLineColor(3);
+    line -> SetLineWidth(2);
+    line -> Draw();
+    cc -> SaveAs((plotPath+plotNamePulseExcludedPdf).c_str());
+    cc -> SaveAs((plotPath+plotNamePulseExcludedRoot).c_str());
+
+    TH2F *h2PulseSelectedWaveforms_ = (TH2F *) file -> Get(("h2PulseSelectedWaveforms_DRS_" + DRS.str() + "_CH_" + CH.str()).c_str());
+    h2PulseSelectedWaveforms_ -> GetXaxis() -> SetNdivisions(712);
+    h2PulseSelectedWaveforms_ -> GetXaxis() -> SetTitle("Time bin");
+    h2PulseSelectedWaveforms_ -> GetYaxis() -> SetTitle("Signal [mV]");
+    h2PulseSelectedWaveforms_ -> GetYaxis() -> SetTitleOffset(1.2);
+    h2PulseSelectedWaveforms_ -> Draw("colz");  
+    cc -> Update();
+    line = new TLine(cfg -> _pulseT0[channel[iChannel].first][channel[iChannel].second].val, gPad->GetUymin(),
+		     cfg -> _pulseT0[channel[iChannel].first][channel[iChannel].second].val, gPad->GetUymax());
+    line -> SetLineColor(2);
+    line -> SetLineWidth(2);
+    line -> Draw();
+    line = new TLine(cfg -> _pulseT0[channel[iChannel].first][channel[iChannel].second].range.min, gPad->GetUymin(),
+		     cfg -> _pulseT0[channel[iChannel].first][channel[iChannel].second].range.min, gPad->GetUymax());
+    line -> SetLineColor(3);
+    line -> SetLineWidth(2);
+    line -> Draw();
+    line = new TLine(cfg -> _pulseT0[channel[iChannel].first][channel[iChannel].second].range.max, gPad->GetUymin(),
+		     cfg -> _pulseT0[channel[iChannel].first][channel[iChannel].second].range.max, gPad->GetUymax());
+    line -> SetLineColor(3);
+    line -> SetLineWidth(2);
+    line -> Draw();
+    cc -> SaveAs((plotPath+plotNamePulseSelectedPdf).c_str());
+    cc -> SaveAs((plotPath+plotNamePulseSelectedRoot).c_str());
+
+    fileLatex << "\\subsection{DUT hit selection on channel DRS " << channel[iChannel].first << " CH " << channel[iChannel].second << " }" << endl;
+    fileLatex << "\\begin{figure}[H]" << endl;
+    fileLatex << "\\centering" << endl;
+    fileLatex << "\\includegraphics[width=0.45\\textwidth]{plots" << plotNameLinearExcludedPdf << "}" << endl;
+    fileLatex << "\\includegraphics[width=0.45\\textwidth]{plots" << plotNameLinearSelectedPdf << "}" << endl;
+    fileLatex << "\\\\" << endl;
+    fileLatex << "\\includegraphics[width=0.45\\textwidth]{plots" << plotNamePulseExcludedPdf << "}" << endl;
+    fileLatex << "\\includegraphics[width=0.45\\textwidth]{plots" << plotNamePulseSelectedPdf << "}" << endl;
+    fileLatex << "\\end{figure}" << endl;
+
+    if(iChannel==1) fileLatex << "\\newpage" << endl;
+  }
+
+  delete cc;
+  file -> Close();
+  delete file;
+  return 0;
+}
+
+int writeDUTConversion(ofstream &fileLatex, 
+		       const char *fileNameDUTConversion, 
+		       const string plotPath){
+
+  fileLatex << "\\newpage" << endl;
+  fileLatex << "\\section{Telescope conversion}" << endl;
+
+  return 0;
+}
+
+int writeTelescopeConversion(ofstream &fileLatex, 
+			     const char *fileNameTelescopeConversion, 
+			     const string plotPath){
+  
+  fileLatex << "\\newpage" << endl;
+  fileLatex << "\\section{Telescope conversion}" << endl;
+
+  return 0;
+}
+
+int writeTelescopeAlignment(ofstream &fileLatex, 
+			    const char *fileNameTelescopeAlignment, 
+			    const string plotPath){
+  
+  fileLatex << "\\newpage" << endl;
+  fileLatex << "\\section{Telescope alignment}" << endl;
+
+  return 0;
+}
+
+int writeTelescopeTracking(ofstream &fileLatex, 
+			   const char *fileNameTelescopeTracking, 
+			   const string plotPath){
+  
+  fileLatex << "\\newpage" << endl;
+  fileLatex << "\\section{Telescope tracking}" << endl;
+
+  return 0;
+}
+
+int writeSynchronization(ofstream &fileLatex, 
+			   const char *fileNameSynchronization, 
+			   const string plotPath){
+  
+  fileLatex << "\\newpage" << endl;
+  fileLatex << "\\section{Synchronization}" << endl;
+
+  return 0;
+}
+
+int writePositionCorrection(ofstream &fileLatex, 
+			   const char *fileNamePositionCorrection, 
+			   const string plotPath){
+  
+  fileLatex << "\\newpage" << endl;
+  fileLatex << "\\section{Position correction}" << endl;
+
+  return 0;
+}
+
+int writeEfficiency(ofstream &fileLatex, 
+			   const char *fileNameEfficiency, 
+			   const string plotPath){
+  
+  fileLatex << "\\newpage" << endl;
+  fileLatex << "\\section{Efficiency}" << endl;
+
+  return 0;
+}
 
 #endif
